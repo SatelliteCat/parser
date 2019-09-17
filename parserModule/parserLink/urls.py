1580bc868f3484f3a45d7dcc97ecd7370112a10a
SatelliteCat <dmn.info@mail.ru>
2019-09-17 22:58:09 +0400

    --

--- parserModule/parserLink/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from parserLink import views
-# from parserLink.models import dbLog
-
-
-urlpatterns = [
-    path("", views.index, name="home"),
-    path("export", views.export, name="export"),
-]
